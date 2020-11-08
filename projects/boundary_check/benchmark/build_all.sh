d61e07fa1189257c12d59940cea35d320b46a63c
Alberto Parravicini <alberto.parravicini@polimi.it>
2020-11-08 15:59:23 +0100

    fixed buildall script for llvm kernels

--- projects/boundary_check/benchmark/build_all.sh
+++ projects/boundary_check/benchmark/build_all.sh
@@ -3,7 +3,7 @@
 echo "Build the CUDA kernel examples using different optimization level"
 
 opt_levels=( O0 O2 )
-simplify=( y n )
+simplify=( y '' )
 
 for o in "${opt_levels[@]}"; do
     for s in "${simplify[@]}"; do
