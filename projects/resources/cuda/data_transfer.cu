2a27e626544e81291e960c5315823caa1b48bfdb
QiZhou1512 <qi1.zhou@mail.polimi.it>
2021-05-22 15:17:23 +0200

    rename policy

--- projects/resources/cuda/data_transfer.cu
+++ projects/resources/cuda/data_transfer.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
-
+#include <nvml.h>
+#include <cuda_runtime.h>
 float p2p_copy (size_t size)
 {
   int *pointers[2];
@@ -75,8 +76,40 @@ void printDeviceAttribute(){
     // printf("concurrent peer access device 0->1: %d \nconcurrent peer access device 1->0: %d\n", can_access_peer_device_0, can_access_peer_device_1);
 
 
+
 }
 
+void nvlinkTest(){
+
+    // unsigned int devices_count;
+
+    // nvmlInit ();
+    // nvmlDeviceGetCount (&devices_count);
+
+    // nvmlDevice_t device;
+    // nvmlDeviceGetHandleByIndex (0, &device);
+
+    // nvmlNvLinkUtilizationControl_t utilization_control;
+    // utilization_control.units = NVML_NVLINK_COUNTER_UNIT_BYTES;
+    // utilization_control.pktfilter = NVML_NVLINK_COUNTER_PKTFILTER_ALL;
+    // nvmlDeviceFreezeNvLinkUtilizationCounter (device, 0, 0, NVML_FEATURE_DISABLED);
+    // nvmlDeviceSetNvLinkUtilizationControl (device, 0, 0, &utilization_control, 1);
+
+    // unsigned long long int tx_before;
+    // unsigned long long int rx_before;
+    // nvmlDeviceGetNvLinkUtilizationCounter (device, 0, 0, &rx_before, &tx_before);
+
+    // // code to measure
+    // float time_third = p2p_copy(1000000000);
+    // printf("time spend %f \n",time_third);
+
+    // unsigned long long int tx_after;
+    // unsigned long long int rx_after;
+    // nvmlDeviceGetNvLinkUtilizationCounter (device, 0, 0, &rx_after, &tx_after);
+
+    // const unsigned long long int tx = tx_after - tx_before;
+    // const unsigned long long int rx = rx_after - rx_before;
+}
 
 int main(){
     size_t size = 1000000000; // 100_000_000_000
