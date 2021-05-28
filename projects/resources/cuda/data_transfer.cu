fbf060a0a2a0aee586faafa258b6af1fb62b8308
QiZhou1512 <qi1512zhou@gmail.com>
2021-05-28 19:31:07 +0000

    nvlink

--- projects/resources/cuda/data_transfer.cu
+++ projects/resources/cuda/data_transfer.cu
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <nvml.h>
 #include <cuda_runtime.h>
+
+#define N 100000000
+
 float p2p_copy (size_t size)
 {
   int *pointers[2];
@@ -35,6 +38,7 @@ float p2p_copy (size_t size)
 
   cudaEventDestroy (end);
   cudaEventDestroy (begin);
+  cudaSetDevice (0);
 
   return elapsed;
 }
@@ -42,7 +46,7 @@ float p2p_copy (size_t size)
 void printDeviceAttribute(){
     int attr_val_device_0 = 0;
     int attr_val_device_1 = 0;
-    cudaError_t err;
+    
     cudaDeviceGetAttribute(&attr_val_device_0,cudaDevAttrConcurrentManagedAccess, 0);
     cudaDeviceGetAttribute(&attr_val_device_1,cudaDevAttrConcurrentManagedAccess, 1);
 
@@ -54,13 +58,6 @@ void printDeviceAttribute(){
     cudaDeviceCanAccessPeer(&can_access_peer_device_1,1,0);
     printf("concurrent peer access device 0->1: %d \nconcurrent peer access device 1->0: %d\n", can_access_peer_device_0, can_access_peer_device_1);
 
-    cudaSetDevice(0);
-    err = cudaDeviceEnablePeerAccess(1, 0);
-    printf("err: %s\n", cudaGetErrorString(err));
-
-    cudaSetDevice(1);
-    err = cudaDeviceEnablePeerAccess(0, 0);
-    printf("err: %s\n", cudaGetErrorString(err));
 
     // cudaSetDevice(0);
     // err = cudaDeviceDisablePeerAccess(1);
@@ -79,47 +76,99 @@ void printDeviceAttribute(){
 
 }
 
-void nvlinkTest(){
+// void nvlinkTest(){
 
-    // unsigned int devices_count;
+//     unsigned int devices_count;
 
-    // nvmlInit ();
-    // nvmlDeviceGetCount (&devices_count);
+//     nvmlInit ();
+//     nvmlDeviceGetCount (&devices_count);
 
-    // nvmlDevice_t device;
-    // nvmlDeviceGetHandleByIndex (0, &device);
+//     nvmlDevice_t device;
+//     nvmlDeviceGetHandleByIndex (0, &device);
 
-    // nvmlNvLinkUtilizationControl_t utilization_control;
-    // utilization_control.units = NVML_NVLINK_COUNTER_UNIT_BYTES;
-    // utilization_control.pktfilter = NVML_NVLINK_COUNTER_PKTFILTER_ALL;
-    // nvmlDeviceFreezeNvLinkUtilizationCounter (device, 0, 0, NVML_FEATURE_DISABLED);
-    // nvmlDeviceSetNvLinkUtilizationControl (device, 0, 0, &utilization_control, 1);
+//     nvmlNvLinkUtilizationControl_t utilization_control;
+//     utilization_control.units = NVML_NVLINK_COUNTER_UNIT_BYTES;
+//     utilization_control.pktfilter = NVML_NVLINK_COUNTER_PKTFILTER_ALL;
+//     nvmlDeviceFreezeNvLinkUtilizationCounter (device, 0, 0, NVML_FEATURE_DISABLED);
+//     nvmlDeviceSetNvLinkUtilizationControl (device, 0, 0, &utilization_control, 1);
 
-    // unsigned long long int tx_before;
-    // unsigned long long int rx_before;
-    // nvmlDeviceGetNvLinkUtilizationCounter (device, 0, 0, &rx_before, &tx_before);
+//     unsigned long long int tx_before;
+//     unsigned long long int rx_before;
+//     nvmlDeviceGetNvLinkUtilizationCounter (device, 0, 0, &rx_before, &tx_before);
 
-    // // code to measure
-    // float time_third = p2p_copy(1000000000);
-    // printf("time spend %f \n",time_third);
+//     // code to measure
+//     float time_third = p2p_copy(1000000000);
+//     printf("time spend %f \n",time_third);
 
-    // unsigned long long int tx_after;
-    // unsigned long long int rx_after;
-    // nvmlDeviceGetNvLinkUtilizationCounter (device, 0, 0, &rx_after, &tx_after);
+//     unsigned long long int tx_after;
+//     unsigned long long int rx_after;
+//     nvmlDeviceGetNvLinkUtilizationCounter (device, 0, 0, &rx_after, &tx_after);
 
-    // const unsigned long long int tx = tx_after - tx_before;
-    // const unsigned long long int rx = rx_after - rx_before;
+//     const unsigned long long int tx = tx_after - tx_before;
+//     const unsigned long long int rx = rx_after - rx_before;
+// }
+
+
+void linktest(){
+    unsigned int devices_count {};
+
+    nvmlInit ();
+    nvmlDeviceGetCount (&devices_count);
+
+    nvmlDevice_t device;
+    nvmlDeviceGetHandleByIndex (1, &device);
+
+    nvmlFieldValue_t field;
+    field.scopeId = 0;
+    field.fieldId = NVML_FI_DEV_NVLINK_THROUGHPUT_DATA_TX;
+    nvmlDeviceGetFieldValues (device, 1, &field);
+    const unsigned long long int initial_tx = field.value.ullVal;
+
+    field.fieldId = NVML_FI_DEV_NVLINK_THROUGHPUT_DATA_RX;
+    nvmlDeviceGetFieldValues (device, 1, &field);
+    const unsigned long long int initial_rx = field.value.ullVal;
+
+    // code to measure
+    float time_third = p2p_copy(N);
+    printf("time spend %f \n",time_third);
+
+    field.fieldId = NVML_FI_DEV_NVLINK_THROUGHPUT_DATA_TX;
+    nvmlDeviceGetFieldValues (device, 1, &field);
+    const unsigned long long int final_tx = field.value.ullVal;
+
+    field.fieldId = NVML_FI_DEV_NVLINK_THROUGHPUT_DATA_RX;
+    nvmlDeviceGetFieldValues (device, 1, &field);
+    const unsigned long long int final_rx = field.value.ullVal;
+
+    const unsigned int rx = final_rx - initial_rx;
+    const unsigned int tx = final_tx - initial_tx;
+    printf("rx : %d\n", rx);
+    printf("tx : %d\n", tx);
 }
 
 int main(){
-    size_t size = 1000000000; // 100_000_000_000
+    size_t size = N; // 1_000_000_000
     float time_first = p2p_copy(size);
     printf("time spend %f \n",time_first);
 
-    float time_second = p2p_copy(size);
-    printf("time spend %f \n",time_second);
+    linktest();
+
     printDeviceAttribute();
-    float time_third = p2p_copy(size);
+    float time_third = p2p_copy(N);
     printf("time spend %f \n",time_third);
+
+    // disable peer access
+    cudaError_t err;
+    cudaSetDevice(0);
+    err = cudaDeviceDisablePeerAccess(1);
+    printf("err: %s\n", cudaGetErrorString(err));
+
+    cudaSetDevice(1);
+    err = cudaDeviceDisablePeerAccess(0);
+    printf("err: %s\n", cudaGetErrorString(err));
+    printDeviceAttribute();
+
+    cudaSetDevice(0);
+    linktest();
     return 0;
 }
--- projects/resources/python/benchmark/test.sh
+++ projects/resources/python/benchmark/test.sh
@@ -1,6 +1,24 @@
-srun graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --grcuda.RetrieveNewStreamPolicy=less_busy --grcuda.RetrieveParentStreamPolicy=disjoint --experimental-options --grcuda.InputPrefetch benchmark_main.py -d -i 5 -n 1000000000 -b b1 --no_cpu_validation;
-srun graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --grcuda.RetrieveNewStreamPolicy=less_busy --grcuda.RetrieveParentStreamPolicy=disjoint --grcuda.InputPrefetch benchmark_main.py -d -i 5 -n 100000000 -b b5 --no_cpu_validation;
-srun graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --grcuda.RetrieveNewStreamPolicy=less_busy --grcuda.RetrieveParentStreamPolicy=disjoint --grcuda.InputPrefetch benchmark_main.py -d -i 5 -n 6000000 -b b6 --no_cpu_validation;
-srun graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --grcuda.RetrieveNewStreamPolicy=less_busy --grcuda.RetrieveParentStreamPolicy=disjoint --grcuda.InputPrefetch benchmark_main.py -d -i 5 -n 20000000 -b b7 --no_cpu_validation;
-srun graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --grcuda.RetrieveNewStreamPolicy=less_busy --grcuda.RetrieveParentStreamPolicy=disjoint --grcuda.InputPrefetch benchmark_main.py -d -i 5 -n 12000 -b b8 --no_cpu_validation;
-srun graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --grcuda.RetrieveNewStreamPolicy=less_busy --grcuda.RetrieveParentStreamPolicy=disjoint --grcuda.InputPrefetch benchmark_main.py -d -i 5 -n 10000 -b b10 --no_cpu_validation;
+#always-new, data_aware, with-const
+#benchmark B1 
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 896 -d -i 5 -n 1000000000 -b b1 --no_cpu_validation;
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=1 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 896 -d -i 5 -n 1000000000 -b b1 --no_cpu_validation;
+
+#benchmark B5
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 100000000 -b b5 --no_cpu_validation;
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=1 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 100000000 -b b5 --no_cpu_validation;
+
+#benchmark B6
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 6000000 -b b6 --no_cpu_validation;
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=1 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 6000000 -b b6 --no_cpu_validation;
+
+#benchmark b7
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 20000000 -b b7 --no_cpu_validation;
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=1 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 20000000 -b b7 --no_cpu_validation;
+
+#benchmark b8
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 12000 -b b8 --no_cpu_validation;
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=1 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 12000 -b b8 --no_cpu_validation;
+
+#benchmark b10
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=2 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 10000 -b b10 --no_cpu_validation;
+graalpython --jvm --polyglot --grcuda.NumberOfGPUs=1 --experimental-options --grcuda.RetrieveNewStreamPolicy=always-new --grcuda.DependencyPolicy=with-const --grcuda.RetrieveParentStreamPolicy=data_aware  benchmark_main.py -g 448 -d -i 5 -n 10000 -b b10 --no_cpu_validation;
