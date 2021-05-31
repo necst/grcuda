aa04e131b7c353a5335b593f04c95eba9a504623
QiZhou1512 <qi1.zhou@mail.polimi.it>
2021-05-31 21:45:26 +0200

    update LU_v2

--- projects/resources/cuda/LU_v2.cu
+++ projects/resources/cuda/LU_v2.cu
@@ -22,9 +22,12 @@ void LU_sequential(float U[M][M], float L[M][M]){
     }
 }
 // call the inner loop M times
-__global__ void LU_parallel(int n,float U[M][M], float L[M][M]){
+__global__ void LU_parallel(int k,int n, float*U, float*L){
     for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x){
-        
+        int v = i/(M-k);
+        L[(k+1)*M + v] = U[(k+1)*M + k]/U[k*M + k];
+        U[(k+1)*M + v] -= L[(k+1)*M + v]*U[k*M + v];
+        printf(" %f\n",L[(k+1)*M + v]);
     }
 }
 
@@ -33,14 +36,55 @@ __global__ void LU_parallel(int n,float U[M][M], float L[M][M]){
 int main(){
     float U[M][M] = {{1,1,1},{2,3,5},{4,6,8}};
     float L[M][M] = {{1,0,0},{0,1,0},{0,0,1}};
+    float U_h[M][M] = {{1,1,1},{2,3,5},{4,6,8}};
+    float L_h[M][M] = {{1,0,0},{0,1,0},{0,0,1}};
     int n = 0;
     for(int i  = 1, k = M; i<M;i++, k--){
         n += (M-i)*k;
     } 
-    
+    float *U_d, *L_d;
+
+    cudaMallocManaged(&U_d,M*M*sizeof(float));
+    cudaMallocManaged(&L_d,M*M*sizeof(float));
+
+    for(int i = 0; i<M;i++){
+        for(int j = 0; j<M;j++){
+            U_d[i*M+j] = U_h[i][j];
+            L_d[i*M+j] = L_h[i][j];
+        }
+    }
+
+    for(int i = 0; i<M*M; i++){
+        printf(" %f", U_d[i]);
+    }
+    int i, j;
+    for(int o = 0; o<M*M; o++){
+        i = o/M;
+        j = o%M;
+
+        if(j == i){
+            L_d[o] = float(1);
+        }else{
+            L_d[o] = float(0);
+        }
+    }
+
+    for( int k = 0; k < M; k++){
+        LU_parallel<<<32,32>>>(k+1, (M-(k+1))*(M-k), U_d, L_d);
+        cudaDeviceSynchronize();
+    }
+
 
 
     printf("n calculated: %d\n",n);
-    LU_sequential(U, L);
+    // LU_sequential(U, L);
+    // for(int i = 0; i<M;i++){
+    //     for(int j = 0; j<M;j++){
+    //         U_d[i*M + j] = U_h[i][j];
+    //         L_d[i*M+j] = L_h[i][j];
 
+    //         printf("%d , %d ", L[i][j], L_d[i*M+j]);
+    //     }
+    // }
+    return 0;
 }
