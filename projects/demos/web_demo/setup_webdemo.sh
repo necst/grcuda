82e15dbbe844698db4d12ef5c7c565d14ca4734e
Francesco Sgherzi <francesco.sgherzi.dev@gmail.com>
2021-08-17 10:47:09 +0200

    Added setup script for OCI

---
+++ projects/demos/web_demo/setup_webdemo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install cmake (required for opencv4nodejs)
+sudo apt install cmake
+
+# Create cloned directory and clone grcuda repo
+mkdir Cloned
+cd Cloned
+git clone https://github.com/AlbertoParravicini/grcuda.git
+cd grcuda
+git checkout GRCUDA-36-web-demo
+
+
+# Move to project directory (backend)
+cd ~/Cloned/grcuda/projects/demos/web_demo/backend
+
+# install project dependencies
+npm i
+
+# Now we should be ready. build and start the server ...
+#npm run build
+
+#node --polyglot --jvm   --vm.Dtruffle.class.path.append=../../../mxbuild/dists/jdk1.8/grcuda.jar
