f3afc090d80a4a6cd3bb7ee1abf144e1bb2cb8b9
AlbertoParravicini <alberto.parravicini@polimi.it>
2021-08-31 16:17:37 +0200

    improved testing interface to test all input options combinations at once

--- projects/com.nvidia.grcuda/src/com/nvidia/grcuda/gpu/stream/RetrieveParentStreamPolicyEnum.java
+++ projects/com.nvidia.grcuda/src/com/nvidia/grcuda/gpu/stream/RetrieveParentStreamPolicyEnum.java
@@ -1,7 +1,11 @@
 package com.nvidia.grcuda.gpu.stream;
 
 public enum RetrieveParentStreamPolicyEnum {
+<<<<<<< HEAD
     SAME_AS_PARENT("same-as-parent"),
+=======
+    DEFAULT("default"),
+>>>>>>> improved testing interface to test all input options combinations at once
     DISJOINT("disjoint");
 
     private final String name;
