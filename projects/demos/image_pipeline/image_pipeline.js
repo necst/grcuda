88b117b66222cb4867185890471820b4cf7f3758
AlbertoParravicini <alberto.parravicini@polimi.it>
2021-09-01 22:03:35 +0200

    added lut to js demo

--- projects/demos/image_pipeline/image_pipeline.js
+++ projects/demos/image_pipeline/image_pipeline.js
@@ -5,7 +5,7 @@ const cv = require("opencv4nodejs");
 // Load function to write to file;
 const fs = require("fs");
 // Load GrCUDA;
-const cu = Polyglot.eval("grcuda", "CU")
+const cu = Polyglot.eval("grcuda", "CU");
 // Load CUDA kernels;
 const ck = require("./cuda_kernels.js");
 
@@ -23,23 +23,25 @@ const RESIZED_IMG_WIDTH_OUT_SMALL = 40;
 const RESIZED_IMG_WIDTH_OUT_LARGE = RESIZED_IMG_WIDTH;
 
 // Build the CUDA kernels;
-const GAUSSIAN_BLUR_KERNEL = cu.buildkernel(ck.GAUSSIAN_BLUR, "gaussian_blur", "const pointer, pointer, sint32, sint32, const pointer, sint32")
-const SOBEL_KERNEL = cu.buildkernel(ck.SOBEL, "sobel", "pointer, pointer, sint32, sint32")
-const EXTEND_KERNEL = cu.buildkernel(ck.EXTEND_MASK, "extend", "pointer, const pointer, const pointer, sint32, sint32")
-const MAXIMUM_KERNEL = cu.buildkernel(ck.EXTEND_MASK, "maximum", "const pointer, pointer, sint32")
-const MINIMUM_KERNEL = cu.buildkernel(ck.EXTEND_MASK, "minimum", "const pointer, pointer, sint32")
-const UNSHARPEN_KERNEL = cu.buildkernel(ck.UNSHARPEN, "unsharpen", "const pointer, const pointer, pointer, float, sint32")
-const COMBINE_KERNEL = cu.buildkernel(ck.COMBINE, "combine", "const pointer, const pointer, const pointer, pointer, sint32")
-const COMBINE_KERNEL_2 = cu.buildkernel(ck.COMBINE_2, "combine_2", "const pointer, const pointer, const pointer, pointer, sint32")
+const GAUSSIAN_BLUR_KERNEL = cu.buildkernel(ck.GAUSSIAN_BLUR, "gaussian_blur", "const pointer, pointer, sint32, sint32, const pointer, sint32");
+const SOBEL_KERNEL = cu.buildkernel(ck.SOBEL, "sobel", "pointer, pointer, sint32, sint32");
+const EXTEND_KERNEL = cu.buildkernel(ck.EXTEND_MASK, "extend", "pointer, const pointer, const pointer, sint32, sint32");
+const MAXIMUM_KERNEL = cu.buildkernel(ck.EXTEND_MASK, "maximum", "const pointer, pointer, sint32");
+const MINIMUM_KERNEL = cu.buildkernel(ck.EXTEND_MASK, "minimum", "const pointer, pointer, sint32");
+const UNSHARPEN_KERNEL = cu.buildkernel(ck.UNSHARPEN, "unsharpen", "const pointer, const pointer, pointer, float, sint32");
+const COMBINE_KERNEL = cu.buildkernel(ck.COMBINE, "combine", "const pointer, const pointer, const pointer, pointer, sint32");
+const COMBINE_KERNEL_LUT = cu.buildkernel(ck.COMBINE_2, "combine_lut", "const pointer, const pointer, const pointer, pointer, sint32, pointer");
 
 // Constant parameters used in the image processing;
-const KERNEL_SMALL_DIAMETER = 3
-const KERNEL_SMALL_VARIANCE = 0.1
-const KERNEL_LARGE_DIAMETER = 7
-const KERNEL_LARGE_VARIANCE = 20
-const KERNEL_UNSHARPEN_DIAMETER = 3
-const KERNEL_UNSHARPEN_VARIANCE = 5
-const UNSHARPEN_AMOUNT = 30
+const KERNEL_SMALL_DIAMETER = 3;
+const KERNEL_SMALL_VARIANCE = 0.1;
+const KERNEL_LARGE_DIAMETER = 7;
+const KERNEL_LARGE_VARIANCE = 20;
+const KERNEL_UNSHARPEN_DIAMETER = 3;
+const KERNEL_UNSHARPEN_VARIANCE = 5;
+const UNSHARPEN_AMOUNT = 30;
+const CDEPTH = 256;
+const FACTOR = 0.8
 // CUDA parameters;
 const BLOCKS = 6;
 const THREADS_1D = 32;
@@ -71,6 +73,45 @@ function gaussian_kernel(buffer, diameter, sigma) {
     }
 }
 
+function lut_r(lut) {
+    for (let i = 0; i < CDEPTH; i++) {
+        x = i / CDEPTH;
+        if (i < CDEPTH / 2) {
+            lut[i] = Math.min(CDEPTH - 1, 255 * (0.8 * (1 / (1 + Math.exp(-x + 0.5) * 7 * FACTOR)) + 0.2)) >> 0;
+        } else {
+            lut[i] = Math.min(CDEPTH - 1, 255 * (1 / (1 + Math.exp((-x + 0.5) * 7 * FACTOR)))) >> 0;
+        }
+    }
+}
+
+function lut_g(lut) {
+    for (let i = 0; i < CDEPTH; i++) {
+        x = i / CDEPTH;
+        y = 0;
+        if (i < CDEPTH / 2) {
+            y = 0.8 * (1 / (1 + Math.exp(-x + 0.5) * 10 * FACTOR)) + 0.2;
+        } else {
+            y = 1 / (1 + Math.exp((-x + 0.5) * 9 * FACTOR));
+        }
+        lut[i] = Math.min(CDEPTH - 1, 255 * Math.pow(y, 1.4)) >> 0;
+    }
+}
+
+function lut_b(lut) {
+    for (let i = 0; i < CDEPTH; i++) {
+        x = i / CDEPTH;
+        y = 0;
+        if (i < CDEPTH / 2) {
+            y = 0.7 * (1 / (1 + Math.exp(-x + 0.5) * 10 * FACTOR)) + 0.3;
+        } else {
+            y = 1 / (1 + Math.exp((-x + 0.5) * 10 * FACTOR));
+        }
+        lut[i] = Math.min(CDEPTH - 1, 255 * Math.pow(y, 1.6)) >> 0;
+    }
+}
+
+const LUT = [lut_r, lut_g, lut_b];
+
 async function storeImageInner(img, imgName, resolution, kind) {
     const imgResized = img.resize(resolution, resolution);
     const buffer = await cv.imencodeAsync('.jpg', imgResized, [cv.IMWRITE_JPEG_QUALITY, 80])
@@ -91,7 +132,7 @@ async function loadImage(imgName) {
 }
 
 // Main processing of the image;
-async function processImage(img, size) {
+async function processImage(img, size, channel) {
     // Allocate image data;
     const image = cu.DeviceArray("int", size, size);
     const image2 = cu.DeviceArray("float", size, size);
@@ -114,6 +155,11 @@ async function processImage(img, size) {
     const blurred_large = cu.DeviceArray("float", size, size);
     const blurred_unsharpen = cu.DeviceArray("float", size, size);
 
+    const lut = cu.DeviceArray("int", CDEPTH);  
+
+    // Initialize the right LUT;
+    LUT[channel](lut);
+
     // Fill the image data;
     const s1 = System.nanoTime();
     image.copyFrom(img, size * size);
@@ -129,18 +175,18 @@ async function processImage(img, size) {
 
     // Main GPU computation;
     // Blur - Small;
-    GAUSSIAN_BLUR_KERNEL((BLOCKS, BLOCKS), (THREADS_2D, THREADS_2D), 4 * KERNEL_SMALL_DIAMETER * KERNEL_SMALL_DIAMETER)(
+    GAUSSIAN_BLUR_KERNEL([BLOCKS, BLOCKS], [THREADS_2D, THREADS_2D], 4 * KERNEL_SMALL_DIAMETER * KERNEL_SMALL_DIAMETER)(
         image, blurred_small, size, size, kernel_small, KERNEL_SMALL_DIAMETER);
     // Blur - Large;
-    GAUSSIAN_BLUR_KERNEL((BLOCKS, BLOCKS), (THREADS_2D, THREADS_2D), 4 * KERNEL_LARGE_DIAMETER * KERNEL_LARGE_DIAMETER)(
+    GAUSSIAN_BLUR_KERNEL([BLOCKS, BLOCKS], [THREADS_2D, THREADS_2D], 4 * KERNEL_LARGE_DIAMETER * KERNEL_LARGE_DIAMETER)(
         image, blurred_large, size, size, kernel_large, KERNEL_LARGE_DIAMETER);
     // Blur - Unsharpen;
-    GAUSSIAN_BLUR_KERNEL((BLOCKS, BLOCKS), (THREADS_2D, THREADS_2D), 4 * KERNEL_UNSHARPEN_DIAMETER * KERNEL_UNSHARPEN_DIAMETER)(
+    GAUSSIAN_BLUR_KERNEL([BLOCKS, BLOCKS], [THREADS_2D, THREADS_2D], 4 * KERNEL_UNSHARPEN_DIAMETER * KERNEL_UNSHARPEN_DIAMETER)(
         image, blurred_unsharpen, size, size, kernel_unsharpen, KERNEL_UNSHARPEN_DIAMETER);
     // Sobel filter (edge detection);
-    SOBEL_KERNEL((BLOCKS, BLOCKS), (THREADS_2D, THREADS_2D))(
+    SOBEL_KERNEL([BLOCKS, BLOCKS], [THREADS_2D, THREADS_2D])(
         blurred_small, mask_small, size, size);
-    SOBEL_KERNEL((BLOCKS, BLOCKS), (THREADS_2D, THREADS_2D))(
+    SOBEL_KERNEL([BLOCKS, BLOCKS], [THREADS_2D, THREADS_2D])(
         blurred_large, mask_large, size, size);
     // Ensure that the output of Sobel is in [0, 1];
     MAXIMUM_KERNEL(BLOCKS * 2, THREADS_1D)(mask_small, maximum_1, size * size);
@@ -156,8 +202,8 @@ async function processImage(img, size) {
     // Combine results;
     COMBINE_KERNEL(BLOCKS * 2, THREADS_1D)(
         image_unsharpen, blurred_large, mask_large, image2, size * size);
-    COMBINE_KERNEL_2(BLOCKS * 2, THREADS_1D)(
-        image2, blurred_small, mask_small, image3, size * size);
+    COMBINE_KERNEL_LUT(BLOCKS * 2, THREADS_1D)(
+        image2, blurred_small, mask_small, image3, size * size, lut);
 
     // Store the image data.
     const tmp = image3[0][0];
@@ -171,7 +217,7 @@ async function processImage(img, size) {
 }
 
 async function processImageBW(img) {
-    return new cv.Mat(Buffer.from(await processImage(img.getData(), img.rows)), img.rows, img.cols, cv.CV_8UC1);
+    return new cv.Mat(Buffer.from(await processImage(img.getData(), img.rows, 0)), img.rows, img.cols, cv.CV_8UC1);
 }
 
 async function processImageColor(img) {
@@ -179,7 +225,7 @@ async function processImageColor(img) {
     // we should process the 3 channels concurrently, and avoid creation of temporary cv.Mat;
     const channels = img.splitChannels();
     for (let c = 0; c < 3; c++) {
-        channels[c] = new cv.Mat(Buffer.from(await processImage(channels[c].getData(), img.rows)), img.rows, img.cols, cv.CV_8UC1);;
+        channels[c] = new cv.Mat(Buffer.from(await processImage(channels[c].getData(), img.rows, c)), img.rows, img.cols, cv.CV_8UC1);;
     }
     return new cv.Mat(channels);  
 }
@@ -216,7 +262,7 @@ async function main() {
     for (let i = 0; i < 20; i++) {
         // Use await for serial execution, otherwise it processes multiple images in parallel.
         // Performance looks identical though;
-        await imagePipeline("lena", i);
+        await imagePipeline("astro1", i);
     }
 }
 
