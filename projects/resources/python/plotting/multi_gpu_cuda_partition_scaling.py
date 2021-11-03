3d01916e2742e5c6cd5e6d57cd718132124b0fee
Alberto Parravicini <alberto.parravicini@polimi.it>
2021-11-03 12:59:36 +0100

    updated partition sclaing

--- projects/resources/python/plotting/multi_gpu_cuda_partition_scaling.py
+++ projects/resources/python/plotting/multi_gpu_cuda_partition_scaling.py
@@ -10,13 +10,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from segretini_matplottini.src.plot_utils import remove_outliers_df_iqr_grouped, compute_speedup_df
+from matplotlib.patches import Patch
+from segretini_matplottini.src.plot_utils import remove_outliers_df_iqr_grouped, compute_speedup_df, save_plot, PALETTE_G3
+from load_data import PLOT_DIR
 
 ##############################
 ##############################
 
 INPUT_DATE = "2021_11_02"
-OUTPUT_DATE = "2021_11_03"
+OUTPUT_DATE = "2021_11_02"
 RES_FOLDER = "../../../../grcuda-data/results/scheduling_multiGPU"
 
 # V100;
@@ -46,34 +48,62 @@ def load_data() -> (pd.DataFrame, pd.DataFrame):
     # Remove outliers;
     remove_outliers_df_iqr_grouped(data, column="computation_sec", group=["size", "gpus", "partitions"],
                                     reset_index=True, quantile=0.9, drop_index=True, debug=True)
-    # Obtain mean of computation times, grouped;
-    data_agg = data.groupby(["size", "gpus", "partitions"]).mean()["computation_sec"].reset_index()
     # Compute speedups;
-    compute_speedup_df(data_agg, key=["size"],
+    compute_speedup_df(data, key=["size"],
                        baseline_filter_col=["gpus", "partitions"], baseline_filter_val=[1, 1],  
                        speedup_col_name="speedup", time_column="computation_sec",
                        baseline_col_name="baseline_sec")
+    # Obtain mean of computation times, grouped;
+    data_agg = data.groupby(["size", "gpus", "partitions"]).mean()["speedup"].reset_index()
     return data, data_agg
 
 
 def plot_scaling(data):
     plt.rcdefaults()
-    sns.set_style("white", {"ytick.left": True, "xtick.bottom": False})
+    sns.set_style("white", {"ytick.left": True, "xtick.bottom": True})
     plt.rcParams["font.family"] = ["Latin Modern Roman Demi"]
     plt.rcParams['hatch.linewidth'] = 0.3
-    plt.rcParams['axes.labelpad'] = 5
+    plt.rcParams['axes.labelpad'] = 3
     plt.rcParams['xtick.major.pad'] = 4.2
     plt.rcParams['ytick.major.pad'] = 1
-    plt.rcParams['axes.linewidth'] = 0.5
+    plt.rcParams['axes.linewidth'] = 1
+    FONTSIZE = 8
+    PALETTE = [PALETTE_G3[i] for i in [1, 2, 4]]
+    PALETTE[0] = "#C5E8C5"
+    fig = plt.figure(figsize=(3.5, 2.5), dpi=600)
+    plt.subplots_adjust(top=0.95,
+                        bottom=0.2,
+                        left=0.15,
+                        right=0.95)
+    ax = sns.lineplot(data=data, x="partitions", y="speedup", hue="gpus", legend=False, palette=PALETTE)
+    # Axes labels;
+    plt.xlabel("Number of partitions")
+    plt.ylabel("Speedup")
+    # Grid and axis limits;
+    ax.yaxis.grid(True, linewidth=0.5)
+    ax.xaxis.grid(False)
+    # Axes limits;
+    ax.set_xlim((data["partitions"].min(), data["partitions"].max()))
+    ax.set_ylim((0.0, 1.75))
+    # Ticks;
+    ax.tick_params(length=4, width=1)
+    ax.yaxis.set_major_locator(plt.LinearLocator(8))
+    ax.set_yticklabels(labels=[f"{l:.2f}x" for l in ax.get_yticks()], ha="right", fontsize=FONTSIZE)
+    x_ticks = sorted(list(data["partitions"].unique()))
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(labels=x_ticks, ha="center",
+                        va="top", rotation=0, fontsize=FONTSIZE)
     
-    fig = plt.figure(figsize=(2, 0.95), dpi=600)
-    plt.subplots_adjust(top=0.78,
-                        bottom=0.17,
-                        left=0.12,
-                        right=0.99,
-                        hspace=0.15,
-                        wspace=0.8)
-    ax = plt.plot(data["partitions"], data["speedup"]) # sns.lineplot(data=data, x="partitions", y="speedup", hue="gpus")
+    # Legend;
+    labels = ["1 GPU", "2 GPUs", "4 GPUs"]
+    patches = [Patch(facecolor=PALETTE[i], edgecolor="#2f2f2f", label=l, linewidth=0.5) for i, l in enumerate(labels)]
+    leg = fig.legend(patches, labels, bbox_to_anchor=(0.95, 0.2), fontsize=FONTSIZE,
+                     ncol=1, loc="lower right", handlelength=1.2, title=None,
+                     handletextpad=0.2, columnspacing=0.5)
+    leg._legend_box.align = "left"
+    leg.get_frame().set_linewidth(0.5)
+    leg.get_frame().set_facecolor('white')
+    
     return fig, ax
     
 
@@ -87,7 +117,7 @@ if __name__ == "__main__":
     data_agg = data_agg[data_agg["size"] == SIZE]
     
     # Plot;
-    fig, ax = plot_scaling(data_agg)
-    
-    
+    fig, ax = plot_scaling(data)    
+    save_plot(PLOT_DIR, "cuda_partition_scaling" + "_{}.{}", date=OUTPUT_DATE, dpi=600)
+
     
