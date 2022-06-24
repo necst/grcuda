7ac016701e90068c8313ce8af4062885f29d66ee
Guido Walter Di Donato <gw.didonato@gmail.com>
2022-06-24 09:25:11 +0200

    Grcuda 96 9 more time logging (#42)
    * moved all mocked computations to a different class
    
    * added mock vec benchmark
    
    * added connection graph dataset with 8 V100s; rounding bandwidth to floor to reduce randomness; added test for vec multigpu
    
    * added mocked b6ml
    
    * added mocked cg-B9 benchmark
    
    * added mmult mocked benchmark
    
    * added partioned z in b11, added kernel for preconditioning in b9; both in cuda
    
    * fixed zpartition in b11 cuda, added zpartition in b11 python
    
    * added preconditioning to b9 python
    
    * fixed device selection policy not using tostring
    
    * updated b11 mocked to use partitioned z
    
    * simplified round-robin policy, now we simply increase the internal state and do a % on the device list
    
    * added min data threshold to consider device for selection
    
    * added option to specify min data threshold
    
    * updated benchmark wrapper to new grcuda policies
    
    * updated python benchmark suite to use current multigpu options
    
    * restored options for experiments
    
    * updated nvprof wrapper
    
    * added connection graphs for 1 and 2 V100
    
    * fixed wrappers
    
    * fixed prefetching in cuda being always on
    
    * fixed benchmarks in python
    
    * added v100 conneciton graph for 4 gpus
    
    * updated path of 8 v100 dataset, added command to create dataset dir in connection graph script
    
    * fixed const flags in py benchmarks multigpu
    
    * fixed kernel timing option being wrong in wrapper
    
    * updated wrapper for testing
    
    * reverted to GraalVM 21.2; fixed performance regression in DeviceArray access by putting logger to static final
    
    * replaced logging strings with lambdas
    
    * optimized init of b1m python
    
    * fixed init of b9 for large matrices
    
    * fixed parameters not being reset
    
    * fixed benchmark parameters
    
    * fixed init in b1. Irrelevant for benchmark performance
    
    * adding heatmap for gpu bandwidth
    
    * adde gpu bandwidth heatmap plot
    
    * updated heatmap, now it's smaller. Updated res loading code. Added loading of new grcuda results
    
    * updated loading of grcuda results
    
    * added grcuda plotting
    
    * added options for a100 to benchmark wrapper
    
    * added logging for multiple computations on same deviceId

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ mx build;
 
 # Install for Java 8+;
 mkdir -p $GRAAL_HOME/languages/grcuda;
-cp $GRCUDA_HOME/mxbuild/dists/grcuda.jar $GRAAL_HOME/languages/grcuda/.;
+cp $GRCUDA_HOME/mxbuild/dists/jdk1.8/grcuda.jar $GRAAL_HOME/languages/grcuda/.;
