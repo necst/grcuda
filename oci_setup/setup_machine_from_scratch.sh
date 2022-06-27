efd581548adfb627b04b92ba116a372dc3bc20ea
Mauro Famà <mauro.fama@mail.polimi.it>
2022-06-27 20:55:22 +0200

    {HOTFIX} Update setup_machine_from_scratch.sh

--- oci_setup/setup_machine_from_scratch.sh
+++ oci_setup/setup_machine_from_scratch.sh
@@ -30,7 +30,7 @@
 #!/bin/bash
 
 # You can use this script to setup a clean machine with Ubuntu 20.04 to use GrCUDA. 
-# We install GraalVM, Nvidia's drivers, CUDA, conda, and download GrCUDA.
+# We install Nvidia's drivers, CUDA, conda, and download GrCUDA.
 # To install GrCUDA, run `cd $GRCUDA_HOME; ./install.sh` after running this script.
 
 # Installation flags (change them to customize your installation);
