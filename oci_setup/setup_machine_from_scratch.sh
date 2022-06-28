9a2972d2603ed1ef41af070c19a13c5ccb957619
Guido Walter Di Donato <gw.didonato@gmail.com>
2022-06-28 21:56:42 +0000

    moved to network version of cuda installer, removed unuseful binary

--- oci_setup/setup_machine_from_scratch.sh
+++ oci_setup/setup_machine_from_scratch.sh
@@ -82,12 +82,10 @@ rm labsjdk-ce-11.0.15+3-jvmci-22.1-b01-linux-amd64.tar.gz
 # -> option 2 (from Nvidia's website).
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
-
 
 # Systems with NVSwitch require Nvidia's fabric manager. 
 # See https://docs.nvidia.com/datacenter/tesla/pdf/fabric-manager-user-guide.pdf
